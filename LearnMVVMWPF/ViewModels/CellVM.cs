545c671b210d61e3b5ac37c8b740efa851b24124
SaintBidies <144172427+SaintBid1es@users.noreply.github.com>
2024-04-20 18:22:16 +0300

    snake

---
+++ LearnMVVMWPF/ViewModels/CellVM.cs
@@ -0,0 +1,32 @@
+﻿using LearnMVVMWPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnMVVMWPF.ViewModels
+{
+    internal class CellVM:BindableBase
+    {
+        public int Row { get; }
+        public int Column { get; }
+
+        private CellType _cellType = CellType.None;
+
+        public CellType CellType
+        {
+            get => _cellType; 
+            set { 
+                _cellType = value;
+                RaisePropertyChanged(nameof(CellType));
+            }
+        }
+        public CellVM(int row, int column)
+        {
+            Row = row;
+            Column = column;
+
+        }
+    }
+}
